0b66ec1adbf1ff13decbaa2520849ba93c12bf1e
imnyoung <hkny0627@gmail.com>
2025-07-06 14:56:07 +0900

    feat: #6.0, #6.1 useEffect

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,17 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("I run all the time");
+  const iRunOnlyOnce = () => {
+    console.log("I run only once");
+  };
+  useEffect(iRunOnlyOnce, []); //useEffect: 해당 코드가 딱 한번만(초기렌더링) 실행될 수 있도록 보호해준다(api호출 등에 사용)
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text="Continue" />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
