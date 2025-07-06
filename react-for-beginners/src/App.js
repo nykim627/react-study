f69975e9c23b1f3e769fd4d5bda0996a6f1442f5
imnyoung <hkny0627@gmail.com>
2025-07-06 16:16:25 +0900

    feat: #6.4 cleanup function

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,32 +1,20 @@
 import { useState, useEffect } from "react";
 
+function Hello() {
+  useEffect(() => {
+    console.log("hi :)");
+    return () => console.log("bye :(");  //cleanup function: component가 destroyed될 때 실행하는 function
+  }, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(() => {
-    console.log("I run only once");
-  }, []); //[](array)가 비어있음 : 해당 코드가 딱 한번만(초기렌더링) 실행됨
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]); //keyword를 잘 지켜보다가 이게 변화할때만 실행될 것이라는 것!
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when 'keyword' & 'counter' changes.");
-  }, [keyword, counter]);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        onChange={onChange}
-        value={keyword}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
