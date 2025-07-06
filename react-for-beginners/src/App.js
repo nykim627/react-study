fb686a2174cfb16983e7f715ea0d680ff8d1dd6f
imnyoung <hkny0627@gmail.com>
2025-07-06 15:54:35 +0900

    feat: #6.2 useEffect > Deps (2)

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -5,16 +5,18 @@ function App() {
   const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
-  console.log("I run all the time");
-  const iRunOnlyOnce = () => {
-    console.log("I run only once");
-  };
-  useEffect(iRunOnlyOnce, []); //useEffect: 해당 코드가 딱 한번만(초기렌더링) 실행될 수 있도록 보호해준다(api호출 등에 사용) <- [](array)가 비어있기 때문
   useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {  //조건 걸어줄 수도 있다.
-      console.log("SEARCH FOR", keyword);
-    }
+    console.log("I run only once");
+  }, []); //[](array)가 비어있음 : 해당 코드가 딱 한번만(초기렌더링) 실행됨
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
   }, [keyword]); //keyword를 잘 지켜보다가 이게 변화할때만 실행될 것이라는 것!
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'keyword' & 'counter' changes.");
+  }, [keyword, counter]);
   return (
     <div>
       <input
