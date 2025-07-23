21a0c870b817c0bf923b4445516215422b4849f9
imnyoung <hkny0627@gmail.com>
2025-07-24 00:18:51 +0900

    feat: # 7.5 React Router

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; //react는 18버전, react-router-dom은 6버전으로 쓰기
 import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/movie" element={<Detail/>}/>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
