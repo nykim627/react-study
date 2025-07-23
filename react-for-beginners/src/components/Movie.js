42d734281d3c942cbb4dabdf11a69046dfcab844
imnyoung <hkny0627@gmail.com>
2025-07-24 00:40:05 +0900

    feat: # 7.6 Parameters

--- react-for-beginners/src/components/Movie.js
+++ react-for-beginners/src/components/Movie.js
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types"; //a href이용시 페이지 새로고침되는데 이건 아님
 
-function Movie({ coverImg, title, summary, genres }) {
+function Movie({ id, coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
-      <h2><Link to="/movie">{title}</Link></h2>
+      <h2><Link to={`/movie/${id}`}>{title}</Link></h2>
       <p>{summary}</p>
       <ul>
         {genres.map((g) => (
@@ -17,6 +17,7 @@ function Movie({ coverImg, title, summary, genres }) {
 }
 
 Movie.propTypes = {
+  id:PropTypes.number.isRequired,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
